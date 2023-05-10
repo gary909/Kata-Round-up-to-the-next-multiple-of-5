671d9b9b5be898276f5669b20bca9d2d14129fad
gary909 <garywhite61@hotmail.com>
2023-05-10 12:18:51 +0200

    code

---
+++ app.js
@@ -0,0 +1,12 @@
+function roundToNext5(n){
+  // ...
+}
+
+console.log(roundToNext5(0)); // 0
+console.log(roundToNext5(2)); // 5
+console.log(roundToNext5(3)); // 5
+console.log(roundToNext5(12)); // 15
+console.log(roundToNext5(21)); // 25
+console.log(roundToNext5(30)); // 30
+console.log(roundToNext5(-2)); // 0
+console.log(roundToNext5(-5)); // -5
